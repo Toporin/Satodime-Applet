f82c9cb37d60f1d9de8061d9a7fee5f59473fd28
Toporin <satochip.wallet@gmail.com>
2025-06-03 12:29:48 +0200

    (debug) try to build NDEF data dynamically directly to buffer
    The NDEF record looks ok but is not recognized as such...

--- src/main/java/org/satodime/applet/SharedObject.java
+++ src/main/java/org/satodime/applet/SharedObject.java
@@ -35,7 +35,8 @@ public class SharedObject {
     private static SharedObject instance;
 
     /** The NDEF data file. Read through the NDEFApplet. **/
-    static final short MAX_NDEF_DATA_FILE_SIZE = 224;
+    //static final short MAX_NDEF_DATA_FILE_SIZE = 224;
+    static final short MAX_NDEF_DATA_FILE_SIZE = 400;
 
     /** base URL for dynamic url **/
     static final byte[] BASE_URL = {'e','x','a','m','p','l','e','.','c','o','m', '/'};
@@ -44,10 +45,12 @@ public class SharedObject {
     static final byte[] HEX = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
     /** NDEF file for dynamic url**/
+    short ndefDataFileSize;
     byte[] ndefDataFile;
 
     /** number of slots **/
     byte nb_slots = 0;
+    static final short SLOT_SIZE = 76; // 38-byte hex-encoded
 
     /** The list of pubkeys for each slot **/
     byte[] ecpubkeys;
@@ -71,9 +74,14 @@ public class SharedObject {
      */
     public SharedObject(byte nb_slots) {
 
+        //this.nb_slots = nb_slots;
+        this.nb_slots = (byte)1; // debug
+
         // Allocate the memory
-        this.ndefDataFile = new byte[MAX_NDEF_DATA_FILE_SIZE];
-        this.nb_slots = nb_slots;
+        this.ndefDataFileSize = getNdefDataSize(nb_slots);
+        //this.ndefDataFile = new byte[MAX_NDEF_DATA_FILE_SIZE];
+        this.ndefDataFile = new byte[this.ndefDataFileSize];
+
         this.ecpubkeys = new byte[(short)(nb_slots * Satodime.SIZE_ECPUBKEY)];
         this.state_array = new byte[nb_slots];
         this.slip44_array= new byte[(short)nb_slots * Satodime.SIZE_SLIP44];
@@ -84,19 +92,243 @@ public class SharedObject {
         if (instance == null) {
             instance = new SharedObject(nb_slots);
         }
+
         return instance;
     }
 
-    public void populateNdefDataFile() {
 
-        byte payloadLength = 0;
-        byte uriIdentifier = 0;
+    public short getNdefDataSize(byte nb_slots){
+        // [header(10b) | base_url() ] +
+        // [nonce(8b) |  authentikey(33b) | nb_slot(1b) |  ] +
+        // [status(1b) | slip44(4b) | pubkey(33b) ] * nb_slot +
+        // [subca_sig_size(1b) | subca_sig(70-72b) | padding(0-2b)] +
+        // [authentikey_sig_size(1b) | authentikey_sig(70-72b) | padding(0-2b)] +
+
+        short size= 0;
+        size+= (short) 10; //header
+        size+= BASE_URL.length; // base URL
+        size+= (short) (42 * 2); // card info, encoded in hex
+        size+= (short) (nb_slots * SLOT_SIZE); // encoded in hex
+        size+= (short) (146); //  signature +  size, encoded in hex, with 0-2b padding after each signature to have a fixed size
+        size+= (short) (146); //  signature +  size, encoded in hex, with 0-2b padding after each signature to have a fixed size
+
+        return size;
+    }
+
+    public short populateNdefDataFile(short offset_start, byte[] buffer) {
+
+        short offset = 0;
+
+        // todo move  to constructor?
+        short offset_first_slot = (short)(10 + BASE_URL.length + 84);
+        short offset_subca_sig = (short)(offset_first_slot + this.nb_slots * SLOT_SIZE);
+        short offset_authentikey_sig = (short)(offset_subca_sig+146);
 
 
-        short offset = (short)3;
+        if (offset_start == 0) {
+
+            // header
+            short ndef_size = getNdefDataSize(this.nb_slots);
+            Util.setShort(buffer, offset, (short)(ndef_size-2));
+            offset += (short) 2;
+            buffer[offset++] = (byte) 0xC1; // Header for Long URL
+            buffer[offset++] = (byte) 0x01; // Type Length
+            buffer[offset++] = (byte)0x00; // Short Payload Length (4b)
+            buffer[offset++] = (byte)0x00; // Short Payload Length (4b)
+            Util.setShort(buffer, offset, (short)(ndef_size-9));
+            offset += (short) 2;
+            buffer[offset++] = (byte) 0x55; // Record Type 'U'
+            buffer[offset++] = (byte) 0x04;// URI Identifier Code (https://)
+
+            // base URL
+            Util.arrayCopy(BASE_URL, (short) (0), buffer, offset, (short) BASE_URL.length);
+            offset += BASE_URL.length;
+
+            // nonce(8b)
+            Util.arrayFillNonAtomic(buffer, offset, (short)16, (byte)'n');
+            offset += (short)16;
+
+            //authentikey(33b)
+            Util.arrayFillNonAtomic(buffer, offset, (short)66, (byte)'a');
+            offset += (short)66;
+
+            // nb_slot(1b)
+            buffer[offset++] = 'n';
+            buffer[offset++] = 'n';
+
+            return offset;
+
+        }
+
+        // slot data
+        for (byte i=0; i<this.nb_slots; i++){
+
+            short offset_slot_i = (short)(offset_first_slot + i*SLOT_SIZE);
+
+            // for slot i
+            if (offset_start == offset_slot_i){
+                //[status(1b) | slip44(4b) | pubkey(33b) ]
+                buffer[offset++] = 's';
+                buffer[offset++] = 's';
+                // slip
+                Util.arrayFillNonAtomic(buffer, offset, (short)8, (byte)'c');
+                offset += (short)8;
+                // pubkey
+                Util.arrayFillNonAtomic(buffer, offset, (short)66, (byte)'p');
+                offset += (short)66;
+
+                return offset;
+            }
+        }
+
+        // subca sig
+        if (offset_start == offset_subca_sig) {
+            // sig length
+            buffer[offset++] = 'l';
+            buffer[offset++] = 'l';
+            // sig + padding
+            Util.arrayFillNonAtomic(buffer, offset, (short)144, (byte)'g'); // hex encoded
+            offset += (short)144;
+
+            return offset;
+        }
+
+        // authentikey sig
+        if (offset_start == offset_authentikey_sig) {
+            // sig length
+            buffer[offset++] = 'l';
+            buffer[offset++] = 'l';
+            // sig + padding
+            Util.arrayFillNonAtomic(buffer, offset, (short)144, (byte)'g'); // hex encoded
+            offset += (short)144;
+
+            return offset;
+        }
+
+        // should not happen
+        return offset;
+    }
+
+    /*
+     *  TEST 0-127 -> 256 hex
+     * */
+
+    public short getNdefDataSize4(byte nb_slot){
+        // example.com/00010203...
+        short size= 0;
+        size+= 10; //header
+        size+= BASE_URL.length; // base URL
+        size+=256; // nb_slots
+
+        return size;
+    }
+
+    public void populateNdefDataFile4(short offset_start) {
+
+        short offset = (short)2;
+        ndefDataFile[offset++] = (byte)0xC1; // Header for Long URL
+        ndefDataFile[offset++] = (byte)0x01; // Type Length
+        ndefDataFile[offset++] = (byte)0x00; // TODO Short Payload Length (4b)
+        ndefDataFile[offset++] = (byte)0x00; // TODO Short Payload Length (4b)
+        ndefDataFile[offset++] = (byte)0x00; // TODO Short Payload Length (4b)
+        ndefDataFile[offset++] = (byte)0x00; // TODO Short Payload Length (4b)
+        ndefDataFile[offset++] = (byte)0x55; // Record Type 'U'
+        ndefDataFile[offset++] = (byte)0x04;// URI Identifier Code (https://)
+        //ndefDataFile[offset++] = ;// URI
+
+        // base URL
+        Util.arrayCopy(BASE_URL, (short)(0), ndefDataFile, offset, (short)BASE_URL.length);
+        offset+=BASE_URL.length;
+
+        //
+        byte tmpbyte;
+        for (short i=0; i<128; i++){
+            // For each byte, convert the two 4-bit nibbles to their hexadecimal value in ascii
+            tmpbyte = (byte)(i & 0xFF);
+            ndefDataFile[offset++] = HEX[(tmpbyte >> 4) & 0x0F];
+            ndefDataFile[offset++] = HEX[tmpbyte & 0x0F];
+        }
+
+        // update ndef length (2bytes)
+        ndefDataFile[(short)0] = (byte) ((short)(offset-2)>>8); // file Length
+        ndefDataFile[(short)1] = (byte) ((offset-2) & 0xFF); // file Length
+
+        // update payload length
+        ndefDataFile[(short)6] = (byte) ((short)(offset-9)>>8); // Payload Length
+        ndefDataFile[(short)7] = (byte) ((offset-9) & 0xFF); // Payload Length
+
+        return;
+    }
+
+/*
+*  TEST 00-> 3F 12_ hex
+* */
+
+    public short getNdefDataSize3(){
+        // example.com/00010203...
+        short size= 0;
+        size+= 7; //header
+        size+= BASE_URL.length; // base URL
+        size+=128; // nb_slots
+
+        return size;
+    }
+
+    public void populateNdefDataFile3(short offset_start) {
+
+        short offset = (short)2;
         ndefDataFile[offset++] = (byte)0xD1; // Header
         ndefDataFile[offset++] = (byte)0x01; // Type Length
-        ndefDataFile[offset++] = (byte)0x00; // TODO Payload Length
+        ndefDataFile[offset++] = (byte)0x00; // TODO Short Payload Length (1b)
+        ndefDataFile[offset++] = (byte)0x55; // Record Type 'U'
+        ndefDataFile[offset++] = (byte)0x04;// URI Identifier Code (https://)
+        //ndefDataFile[offset++] = ;// URI
+
+        // base URL
+        Util.arrayCopy(BASE_URL, (short)(0), ndefDataFile, offset, (short)BASE_URL.length);
+        offset+=BASE_URL.length;
+
+        //
+        byte tmpbyte;
+        for (short i=0; i<64; i++){
+            // For each byte, convert the two 4-bit nibbles to their hexadecimal value in ascii
+            tmpbyte = (byte)(i & 0xFF);
+            ndefDataFile[offset++] = HEX[(tmpbyte >> 4) & 0x0F];
+            ndefDataFile[offset++] = HEX[tmpbyte & 0x0F];
+        }
+
+        // update ndef length (2bytes)
+        ndefDataFile[(short)0] = (byte) ((short)(offset-2)>>8); // file Length
+        ndefDataFile[(short)1] = (byte) ((offset-2) & 0xFF); // file Length
+
+        // update payload length
+        ndefDataFile[(short)4] = (byte) (offset-6); // Payload Length
+
+        return;
+    }
+
+/*
+*       TEST NB_SLOT
+* */
+
+    public short getNdefDataSize_a(byte nb_slot){
+        short size= 0;
+        size+= 7; //header
+        size+= BASE_URL.length; // base URL
+        //size++; // nb_slots
+        //size++; // nb_slots
+
+
+        return size;
+    }
+
+    public void populateNdefDataFile_a(short offset_start) {
+
+
+        short offset = (short)2;
+        ndefDataFile[offset++] = (byte)0xD1; // Header
+        ndefDataFile[offset++] = (byte)0x01; // Type Length
+        ndefDataFile[offset++] = (byte)0x00; // TODO Short Payload Length (1b)
         ndefDataFile[offset++] = (byte)0x55; // Record Type 'U'
         ndefDataFile[offset++] = (byte)0x04;// URI Identifier Code (https://)
         //ndefDataFile[offset++] = ;// URI
@@ -106,31 +338,21 @@ public class SharedObject {
         offset+=BASE_URL.length;
 
         // nb_slots as one hex character
-        ndefDataFile[offset++] = HEX[nb_slots & 0x0F]; // nb_slots should be <= 0x0F!
+        //ndefDataFile[offset++] = HEX[nb_slots & 0x0F]; // nb_slots should be <= 0x0F!
 
-        // ecpubkey
-//        //Util.arrayCopy(ecpubkeys, (short)(0), ndefDataFile, offset, ecpubkeys.length);
-//        byte tmpbyte;
-//        for (short i=0; i<ecpubkeys.length; i++){
-//            // For each byte, convert the two 4-bit nibbles to their hexadecimal value in ascii
-//            tmpbyte = ecpubkeys[i];
-//            ndefDataFile[offset++] = HEX[(tmpbyte >> 4) & 0x0F];
-//            ndefDataFile[offset++] = HEX[tmpbyte & 0x0F];
-//        }
-
-        // update array length
-        ndefDataFile[(short)0] = (byte) (offset-1); // file Length
+        // offset_start as 2 hex characters
+        //ndefDataFile[offset++] = HEX[(short)((offset_start>>4) & 0x0F)]; // nb_slots should be <= 0x0F!
+        //ndefDataFile[offset++] = HEX[offset_start & 0x0F]; // nb_slots should be <= 0x0F!
 
         // update ndef length (2bytes)
-        ndefDataFile[(short)1] = (byte) ((short)(offset-3)>>8); // file Length
-        ndefDataFile[(short)2] = (byte) ((offset-3) & 0xFF); // file Length
+        ndefDataFile[(short)0] = (byte) ((short)(offset-2)>>8); // file Length
+        ndefDataFile[(short)1] = (byte) ((offset-2) & 0xFF); // file Length
 
         // update payload length
-        ndefDataFile[(short)5] = (byte) (offset-7); // Payload Length
+        ndefDataFile[(short)4] = (byte) (offset-6); // Payload Length
 
         return;
     }
-
 
 
 }
